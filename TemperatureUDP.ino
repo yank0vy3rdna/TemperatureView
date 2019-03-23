9fac10ce73eac600748c0f5337b9c31bec6d6c15
Bibiw1 <bibiwa2001@gmail.com>
2019-03-23 12:16:11 +0300

    Update TemperatureUDP.ino

--- TemperatureUDP.ino
+++ TemperatureUDP.ino
@@ -30,10 +30,55 @@ void setup(void)
   // Инициализация датчиков
   sensors.begin();
   devcount = sensors.getDeviceCount();
-  Serial.print(devcount, DEC);
+  Serial.println(devcount, DEC);
   oneWire.reset_search();
+  Thermometer[0][0]=40;
+  Thermometer[0][1]=255;
+  Thermometer[0][2]=220;
+  Thermometer[0][3]=8;
+  Thermometer[0][4]=74;
+  Thermometer[0][5]=4;
+  Thermometer[0][6]=0;
+  Thermometer[0][7]=75;
+  
+  Thermometer[1][0]=40; 
+  Thermometer[1][1]=255;
+  Thermometer[1][2]=188;
+  Thermometer[1][3]=107;
+  Thermometer[1][4]=76;
+  Thermometer[1][5]=4;
+  Thermometer[1][6]=0;
+  Thermometer[1][7]=147;
+  
+  Thermometer[2][0]=40;
+  Thermometer[2][1]=255;
+  Thermometer[2][2]=1;
+  Thermometer[2][3]=9;
+  Thermometer[2][4]=74;
+  Thermometer[2][5]=4;
+  Thermometer[2][6]=0;
+  Thermometer[2][7]=118;
+  
+  Thermometer[3][0]=40;
+  Thermometer[3][1]=255;
+  Thermometer[3][2]=230;
+  Thermometer[3][3]=8;
+  Thermometer[3][4]=74;
+  Thermometer[3][5]=4;
+  Thermometer[3][6]=0;
+  Thermometer[3][7]=114;
+  
+  Thermometer[4][0]=40;
+  Thermometer[4][1]=255;
+  Thermometer[4][2]=58;
+  Thermometer[4][3]=89;
+  Thermometer[4][4]=78;
+  Thermometer[4][5]=4;
+  Thermometer[4][6]=0;
+  Thermometer[4][7]=196;
   for (int i = 0; i < devcount; i++) {
-    oneWire.search(Thermometer[i]);
+    //oneWire.search(Thermometer[i]);
+    Serial.println(Thermometer[i][0]);
     sensors.setResolution(Thermometer[i], TEMPERATURE_PRECISION);
     Serial.print("Device ");
     Serial.print(i);
@@ -55,7 +100,8 @@ void printAddress(DeviceAddress deviceAddress)
   {
     // zero pad the address if necessary
     if (deviceAddress[i] < 16) Serial.print("0");
-    Serial.print(deviceAddress[i], HEX);
+    Serial.print(deviceAddress[i], DEC);
+    Serial.print(" ");
   }
 }
 
@@ -95,41 +141,7 @@ void loop()
     ReplyBuffer[i] = 0;
   }
 
-  if (packetSize == 4) {
-    
-    IPAddress remote = Udp.remoteIP(); //Сохраняю IP сервера
-
-    sensors.requestTemperatures();//Запрашиваю датчики
-    
-    Serial.println(" --- ");
-    
-    printTemperature(Thermometer[1]);
-    
-    float tempC = sensors.getTempC(Thermometer[1]); //Запрос температуры
-
-    byte znak = 0;
-    
-    if (tempC > 0) {
-      znak = 1;
-    } else {
-      tempC = tempC * -1;
-    }
-    
-    byte cel = (byte)tempC;//Целое значение
-    
-    byte drob = (byte)((tempC - cel) * 100); //Дробное значение
-    
-    ReplyBuffer[0] = cel;
-    ReplyBuffer[1] = drob;
-    ReplyBuffer[2] = znak;
-
-    Serial.println(" --- ");
-    
-    Udp.beginPacket(remote, PORT);//отправка
-    Udp.write(ReplyBuffer, UDP_TX_PACKET_MAX_SIZE_MY);
-    Udp.endPacket();
-    
-  }else if (packetSize != 0) //если пришел пакет, читаю его
+  if (packetSize != 0) //если пришел пакет, читаю его
   {
 
     IPAddress remote = Udp.remoteIP(); //Сохраняю IP сервера
